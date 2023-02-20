4cb650a4561a0c1cd883d05eef32a7babab66378
sanjmgr <sanjmgr05@gmail.com>
2023-02-20 16:40:47 +0545

    added jwt

---
+++ src/main/java/com/sanjmgr/security/auth/AuthenticationService.java
@@ -0,0 +1,67 @@
+package com.sanjmgr.security.auth;
+
+import com.sanjmgr.security.config.JwtService;
+import com.sanjmgr.security.user.Role;
+import com.sanjmgr.security.user.User;
+import com.sanjmgr.security.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+
+        User user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+
+        String token = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        /*User user = (User) authentication.getPrincipal();*/
+
+        User user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String token = jwtService.generateToken(user);
+
+
+
+        /*Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsernameOrEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = (User) authentication.getPrincipal();
+        var jwtToken = jwtService.generateJwt(user);*/
+
+
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .build();
+    }
+}
---
+++ src/main/java/com/sanjmgr/security/user/Role.java
@@ -0,0 +1,5 @@
+package com.sanjmgr.security.user;
+
+public enum Role {
+    USER, ADMIN
+}
